e3c73e949d5c9d73c64f30cd30bd31f6ccb3f034
Dawid Kołba <kolba.dawid@gmail.com>
2024-10-10 18:06:43 +0200

    initial first logic, added template

---
+++ TeaShopDemo/TeaShopDemo/Controllers/Store.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeaShopDemo.Controllers
+{
+    public class Store : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
