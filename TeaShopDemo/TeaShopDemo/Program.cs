a96dfe9c9cc01653886b84d41c2df04a27b51661
Dawid Kołba <kolba.dawid@gmail.com>
2024-10-13 18:08:41 +0200

    user accounts

--- TeaShopDemo/TeaShopDemo/Program.cs
+++ TeaShopDemo/TeaShopDemo/Program.cs
@@ -1,7 +1,40 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TeaShopDemo.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("TeaShopDemoContextConnection") ?? throw new InvalidOperationException("Connection string 'TeaShopDemoContextConnection' not found.");
+
+builder.Services.AddDbContext<TeaShopDemoContext>(options => options.UseSqlite(connectionString));
 
 // Add services to the container.
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    options.CheckConsentNeeded = context => true;
+});
+
 builder.Services.AddControllersWithViews();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddDefaultUI()
+    .AddDefaultTokenProviders()
+    .AddEntityFrameworkStores<TeaShopDemoContext>();
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("RequireAdministratorRole",
+        policy => policy.RequireRole("Administrator"));
+});
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Identity/Account/Login";
+});
+
+builder.Services.AddSession(options =>
+{
+    options.Cookie.Name = "TeaShopDemo.session";
+    options.IdleTimeout = TimeSpan.FromMinutes(60);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
 var app = builder.Build();
 
@@ -14,14 +47,17 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCookiePolicy();
+app.UseSession();
+
 app.UseStaticFiles();
-
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 
 app.Run();
